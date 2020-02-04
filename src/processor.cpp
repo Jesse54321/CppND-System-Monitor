80f221a263756b644baa24f8da11434a010a2b81
Jesse54321 <48259364+Jesse54321@users.noreply.github.com>
2020-02-04 09:47:26 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,17 @@
+#include <string>
+#include <vector>
+
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+    std::vector<std::string> cpu_values = LinuxParser::CpuUtilization();
+    
+    float idle = std::stof(cpu_values[3]) + std::stof(cpu_values[4]);
+    float non_idle = std::stof(cpu_values[0]) + std::stof(cpu_values[1]) + std::stof(cpu_values[2]) + std::stof(cpu_values[5]) + std::stof(cpu_values[6]) + std::stof(cpu_values[7]);
+
+    float total = idle + non_idle;
+
+    return (total - idle) / total; 
+}
